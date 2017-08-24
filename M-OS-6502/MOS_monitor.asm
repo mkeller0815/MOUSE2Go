de32dd906626a5e2bb7708f23f1da01ae8f5e4dc
Mario Keller <mario@k-networking.de>
2017-08-24 07:28:06 +0200

    optimization and new monitor command s (show byte)

--- M-OS-6502/MOS_monitor.asm
+++ M-OS-6502/MOS_monitor.asm
@@ -18,11 +18,11 @@
 .scope
 
 m_start:
-.invoke print MS_WELCOME    ; print welcome
+    .invoke print MS_WELCOME    ; print welcome
+m_main:             ; monitor main loop
     jsr m_clear_buffer  ; clear input buffer
     jsr m_show_prompt   ; show initial prompt
 
-m_main:             ; monitor main loop
 
 _wait:
     jsr j_rchr          ; read character
@@ -38,7 +38,7 @@ _wait:
     stx K_BUF_P         ; save buffer pointer
     cpx #K_BUF_LEN      ; check for end of buffer
     beq m_parse         ; if end of buffer -> parse
-    jmp m_main          ; next character
+    jmp _wait          ; next character
 
 m_parse:
                         ; code for parsing commands here
@@ -72,16 +72,14 @@ m_cmdjmp:
     sta K_VAR1_H
     jmp (K_VAR1_L)       ; jump to command
 m_parse_end:
-    jsr m_clear_buffer   ; clear input buffer
-    jsr m_show_prompt    ; show new prompt
-    jmp m_main           ; back to mainloop
+    jmp m_main          ; back to mainloop
 
 .scend
 
 m_cmd_num:
-    .byte   11
+    .byte   12
 m_cmd_list:
-    .byte "acdfghimorv"
+    .byte "acdfghimorsv"
 
 m_cmd_jumptable:
     .word m_cmd_assemble
@@ -94,6 +92,7 @@ m_cmd_jumptable:
     .word m_cmd_memdump
     .word m_cmd_output
     .word m_cmd_reset
+    .word m_cmd_show
     .word m_cmd_vtl2
 
 
@@ -106,6 +105,22 @@ m_cmd_vtl2:
 m_cmd_reset:
     jmp ($fffc)     ; jump to reset vector
 
+
+m_cmd_show:
+    ldx #$02            ; bad hack to skip first blank
+    jsr j_a2b           ; parse high byte
+    sta K_VAR1_H        ; store high byte
+    jsr j_a2b           ; parse low byte
+    sta K_VAR1_L        ; store low byte
+    .invoke linefeed
+    ldx #0
+    lda (K_VAR1_L,X)    ; load value
+    pha
+    jsr j_hex8out       ; show hex value
+    .invoke space
+    pla
+    jsr j_bin8out
+    jmp m_main     ; back to parse
 
 m_cmd_memdump:
     ldx #$02            ; bad hack to skip first blank
