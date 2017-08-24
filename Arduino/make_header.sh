842bde713ca103c6e8c90c45e019b8e944884afd
Mario Keller <mario@k-networking.de>
2017-08-24 13:42:22 +0200

    initial site

--- Arduino/make_header.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# create C-Header out of the binary image
-xxd -i ROMIMAGE.bin > arduino_6502_mouse/rom_image.h
-
-
-# now adjust the declartion part
-sed -i '' 's/unsigned char ROMIMAGE_bin\[\] =/const uchar BIOS\[8192\] PROGMEM =/g' arduino_6502_mouse/rom_image.h
-
--- M-OS-6502/make.sh
+++
@@ -1,5 +0,0 @@
-export PYTHONPATH=/usr/local/lib/python2.7/site-packages/
-
-ophis -l listfile.txt -m labelmap.txt -o MOUSE_ROM.bin MOS_main.asm 
-#py65mon -m 65c02 -r ophis.bin 
-
